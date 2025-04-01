8edcf1fd4c05096a60254b916d6b08ed2edb08f5
Koh Jun Jie <koh.jun.jie@outlook.com>
2025-04-01 13:00:51 +0800

    removed wc code from main.go

--- wc/main.go
+++ wc/main.go
@@ -3,23 +3,39 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
+
+	count "github.com/kjj1998/coding-challenges/wc/count"
 )
+
+var BUFFER_SIZE int = 32 * 1024
 
 func main() {
 	bytesFlag := flag.Bool("c", false, "Count the number of bytes in the file")
-	flag.Parse()
+	linesFlag := flag.Bool("l", false, "Count the number of lines in the file")
+	wordsFlag := flag.Bool("w", false, "Count the number of words in the file")
 
+	flag.Parse()
 	files := flag.Args()
 
+	if len(files) != 1 {
+		log.Fatal("No file supplied\n")
+	}
 	file, err := os.Open(files[0])
+
 	if err != nil {
 		panic(err)
 	}
 
-	stat, _ := file.Stat()
-
 	if *bytesFlag {
-		fmt.Printf("%d %s\n", stat.Size(), files[0])
+		numberOfBytes := count.CountNumberOfBytes(file)
+		fmt.Printf("%d %s\n", numberOfBytes, files[0])
+	} else if *linesFlag {
+		numberOfLines := count.CountNumberOfLines(file)
+		fmt.Printf("%d %s\n", numberOfLines, files[0])
+	} else if *wordsFlag {
+		numberOfWords := count.CountNumberOfWords(file)
+		fmt.Printf("%d %s\n", numberOfWords, files[0])
 	}
 }
