abda2b9be185a2c82c05cc13d9de0d04eed373d4
Koh Jun Jie <koh.jun.jie@outlook.com>
2025-04-13 21:35:50 +0800

    added default option

--- wc/main.go
+++ wc/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
 	"os"
 
 	count "github.com/kjj1998/coding-challenges/wc/count"
@@ -19,34 +18,36 @@ func main() {
 
 	flag.Parse()
 	files := flag.Args()
+	var reader *os.File
+	var fileName string
 
-	if len(files) != 1 {
-		log.Fatal("No file supplied\n")
-	}
-	file, err := os.Open(files[0])
-
-	if err != nil {
-		panic(err)
+	if len(files) == 1 {
+		file, _ := os.Open(files[0])
+		reader = file
+		fileName = files[0]
+	} else {
+		reader = os.Stdin
+		fileName = ""
 	}
 
 	if *bytesFlag {
-		numberOfBytes := count.CountNumberOfBytes(file)
-		fmt.Printf("%d %s\n", numberOfBytes, files[0])
+		numberOfBytes := count.CountNumberOfBytes(reader)
+		fmt.Printf("%d %s\n", numberOfBytes, fileName)
 	} else if *linesFlag {
-		numberOfLines := count.CountNumberOfLines(file)
-		fmt.Printf("%d %s\n", numberOfLines, files[0])
+		numberOfLines := count.CountNumberOfLines(reader)
+		fmt.Printf("%d %s\n", numberOfLines, fileName)
 	} else if *wordsFlag {
-		numberOfWords := count.CountNumberOfWords(file)
-		fmt.Printf("%d %s\n", numberOfWords, files[0])
+		numberOfWords := count.CountNumberOfWords(reader)
+		fmt.Printf("%d %s\n", numberOfWords, fileName)
 	} else if *charactersFlag {
-		numberOfCharacters := count.CountNumberOfCharacters(file)
-		fmt.Printf("%d %s\n", numberOfCharacters, files[0])
+		numberOfCharacters := count.CountNumberOfCharacters(reader)
+		fmt.Printf("%d %s\n", numberOfCharacters, fileName)
 	} else {
-		numberOfBytes := count.CountNumberOfBytes(file)
-		numberOfLines := count.CountNumberOfLines(file)
-		file.Seek(0, 0)
-		numberOfWords := count.CountNumberOfWords(file)
+		numberOfBytes := count.CountNumberOfBytes(reader)
+		numberOfLines := count.CountNumberOfLines(reader)
+		reader.Seek(0, 0)
+		numberOfWords := count.CountNumberOfWords(reader)
 
-		fmt.Printf("%d %d %d %s\n", numberOfBytes, numberOfLines, numberOfWords, files[0])
+		fmt.Printf("%d %d %d %s\n", numberOfLines, numberOfWords, numberOfBytes, fileName)
 	}
 }
