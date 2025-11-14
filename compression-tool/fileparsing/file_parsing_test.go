486f4deecc1334b4edb232cb960538097aa6b3b6
Koh Jun Jie <koh.jun.jie@outlook.com>
2025-11-14 21:43:15 +0800

    fixed file parsing tests

--- compression-tool/fileparsing/file_parsing_test.go
+++ compression-tool/fileparsing/file_parsing_test.go
@@ -24,20 +24,11 @@ func TestParsefile(t *testing.T) {
 		}
 	})
 
-	t.Run("parse file and get frequency of the X character", func(t *testing.T) {
+	t.Run("parse file and get the contents of the text", func(t *testing.T) {
 		got, _ := ParseFile("../files/test.txt")
-		want := 333
+		want := "\ufeffThe Project Gutenberg eBook of Les Misérables, by Victor Hugo\r\n\r\nThis eBook is for the use of anyone anywhere in the United States and\r\nmost other parts of the world at no cost and with almost no restrictions\r\nwhatsoever. You may copy it, give it away or re-use it under the terms\r\nof the Project Gutenberg License included with this eBook or online at\r\nwww.gutenberg.org. If you are not located in the United States, you\r\nwill have to check the laws of the country where you are located before\r\nusing this eBook. Hello World!"
 
-		if got['X'] != want {
-			t.Errorf("got %q want %q", got, want)
-		}
-	})
-
-	t.Run("parse file and get frequency of the t character", func(t *testing.T) {
-		got, _ := ParseFile("../files/test.txt")
-		want := 223000
-
-		if got['t'] != want {
+		if got != want {
 			t.Errorf("got %q want %q", got, want)
 		}
 	})
