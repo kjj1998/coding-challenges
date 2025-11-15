45757de458a77c31ad1a39bfbfba2b968445bef9
Koh Jun Jie <koh.jun.jie@outlook.com>
2025-11-15 15:47:05 +0800

    downloaded test files and tried out flags in main.go

--- cat/main.go
+++ cat/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("initialized cat package")
+	fieldPos := flag.String("f", "1", "The position of the field to read, defaults to 1 for the first field")
+	file := flag.String("file", "", "The name of the file to read from, defaults to empty string")
+
+	flag.Parse()
+
+	fmt.Printf("fieldPos: %s, file: %s\n", *fieldPos, *file)
 }
