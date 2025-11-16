a099e8178106ce0dd91c0656536d12f1a89e4028
Koh Jun Jie <koh.jun.jie@outlook.com>
2025-11-16 17:12:36 +0800

    changed filename from flag to positional argument

--- cut/main.go
+++ cut/main.go
@@ -10,11 +10,16 @@ import (
 func main() {
 	fieldPos := flag.String("f", "1", "The position of the field to read, defaults to 1 for the first field")
 	delimiter := flag.String("d", "\t", "the delimiter used to separate the text in the file, defaults to tab")
-	file := flag.String("file", "", "The name of the file to read from, defaults to empty string")
 
 	flag.Parse()
 
-	values := tool.Cut(fieldPos, delimiter, file)
+	// Get file name from positional arguments
+	var file string
+	if len(flag.Args()) > 0 {
+		file = flag.Args()[0]
+	}
+
+	values := tool.Cut(fieldPos, delimiter, &file)
 
 	for _, val := range values {
 		fmt.Println(val)
