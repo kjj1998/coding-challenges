714d34e2af7ad4128b3627e74a6e04701303d2ac
Koh Jun Jie <koh.jun.jie@outlook.com>
2025-11-16 20:19:10 +0800

    completed step 3 of cut

--- cut/main.go
+++ cut/main.go
@@ -8,7 +8,8 @@ import (
 )
 
 func main() {
-	fieldPos := flag.String("f", "1", "The position of the field to read, defaults to 1 for the first field")
+	fieldPos := flag.String("f", "1", `The position(s) of the field to read, defaults to 1 for the first field. 
+	To read multiple fields, input them separated by commas as in 1,2 or separated by whitespaces as in \"1 2 3\"`)
 	delimiter := flag.String("d", "\t", "the delimiter used to separate the text in the file, defaults to tab")
 
 	flag.Parse()
@@ -19,9 +20,26 @@ func main() {
 		file = flag.Args()[0]
 	}
 
-	values := cut.Cut(fieldPos, delimiter, &file)
+	fieldPositions := cut.ParseFieldPos(fieldPos)
+	cutValues := cut.Cut(fieldPositions, delimiter, &file)
+	maxLen := 0
 
-	for _, val := range values {
-		fmt.Println(val)
+	for _, value := range cutValues {
+		if len(value) > maxLen {
+			maxLen = len(value)
+		}
+	}
+
+	for i := 0; i < maxLen; i++ {
+		for j, value := range cutValues {
+			if len(value) > i {
+				fmt.Printf("%s", value[i])
+
+				if j < len(cutValues)-1 {
+					fmt.Printf("%s", *delimiter)
+				}
+			}
+		}
+		fmt.Println()
 	}
 }
--- cut/tool/cut.go
+++ cut/tool/cut.go
@@ -8,7 +8,7 @@ import (
 	"strings"
 )
 
-func Cut(fieldPos *string, delimiter *string, filePath *string) []string {
+func Cut(fieldPositions []int, delimiter *string, filePath *string) [][]string {
 	file, err := os.Open(*filePath)
 	if err != nil {
 		log.Fatalf("Error opening file: %v", err)
@@ -36,14 +36,48 @@ func Cut(fieldPos *string, delimiter *string, filePath *string) []string {
 		log.Fatalf("Error reading file: %v", err)
 	}
 
-	fieldPosInteger, err := strconv.Atoi(*fieldPos)
-	if err != nil {
-		log.Fatalf("Error converting %s to integer: %v", *fieldPos, err)
+	for _, position := range fieldPositions {
+		if position < 1 || position > len(data) {
+			log.Fatalf("Field position value out of bounds")
+		}
 	}
 
-	if fieldPosInteger < 1 || fieldPosInteger > len(data) {
-		log.Fatalf("Field position value out of bounds")
+	var cutValues [][]string
+
+	for _, position := range fieldPositions {
+		cutValues = append(cutValues, data[position-1]) // position - 1 because user supplied field positions start as 1 instead of 0
 	}
 
-	return data[fieldPosInteger-1]
+	return cutValues
+}
+
+func checkFieldPositions(fieldPositions []string) []int {
+	intPositions := []int{}
+
+	for _, position := range fieldPositions {
+		intPosition, err := strconv.Atoi(position)
+		if err != nil {
+			log.Fatal("Only integer values can be entered for field positions\n")
+		}
+		intPositions = append(intPositions, intPosition)
+	}
+
+	return intPositions
+}
+
+func ParseFieldPos(fieldPos *string) []int {
+	if len(*fieldPos) == 1 {
+		return checkFieldPositions([]string{*fieldPos})
+	}
+
+	if strings.ContainsRune(*fieldPos, ',') {
+		return checkFieldPositions(strings.Split(*fieldPos, ","))
+	}
+
+	if strings.ContainsRune(*fieldPos, ' ') {
+		return checkFieldPositions(strings.Split(*fieldPos, " "))
+	}
+
+	log.Fatalf("Invalid field position(s) entered: %s\n", *fieldPos)
+	return nil
 }
