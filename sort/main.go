33ca16fe3b3fa7a36897867278fac191a169bfa6
Koh Jun Jie <koh.jun.jie@outlook.com>
2025-11-30 11:21:13 +0800

    completed step 1 of sort challenge

--- sort/main.go
+++ sort/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"slices"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	flag.Parse()
+
+	var filePath string
+	if len(flag.Args()) > 0 {
+		filePath = flag.Args()[0]
+	}
+
+	var scanner *bufio.Scanner
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Fatalf("Error opening file: %v", err)
+	}
+	defer file.Close()
+	scanner = bufio.NewScanner(file)
+
+	strs := []string{}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		strs = append(strs, line)
+	}
+
+	slices.Sort(strs)
+
+	for _, str := range strs {
+		fmt.Println(str)
+	}
 }
