1347a7148fa35720acf7f4bb40ede99a0c309695
Koh Jun Jie <koh.jun.jie@outlook.com>
2025-12-13 11:48:34 +0800

    added test files for merge sort

---
+++ sort/algo/mergesort_test.go
@@ -0,0 +1,30 @@
+package algo
+
+import (
+	"slices"
+	"testing"
+)
+
+func TestMergeSort(t *testing.T) {
+	t.Run("Sort an array of strings", func(t *testing.T) {
+		testStr := []string{"banana", "app", "apple", "bat"}
+
+		got := MergeSort(testStr)
+		want := []string{"app", "apple", "banana", "bat"}
+
+		if !slices.Equal(got, want) {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("Sort an array of strings with duplicates", func(t *testing.T) {
+		testStr := []string{"bat", "banana", "app", "apple", "bat", "app"}
+
+		got := MergeSort(testStr)
+		want := []string{"app", "app", "apple", "banana", "bat", "bat"}
+
+		if !slices.Equal(got, want) {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+}
